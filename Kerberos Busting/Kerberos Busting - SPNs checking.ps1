50193c0863fb59a5adc3f33b1e987015879c50c1
Itay Bar <95435881+Itayb3@users.noreply.github.com>
2023-08-28 17:15:18 +0300

    Add files via upload

---
+++ Kerberos Busting/Kerberos Busting - SPNs checking.ps1
@@ -0,0 +1,26 @@
+﻿Import-Module ActiveDirectory
+$results = @()
+Get-ADUser -Filter {ServicePrincipalName -like "*"} -Property ServicePrincipalName | ForEach-Object {
+    foreach ($spn in $_.ServicePrincipalName) {
+        $results += [PSCustomObject]@{
+            'Type'  = 'User'
+            'Name'  = $_.Name
+            'SPN'   = $spn
+        }
+    }
+}
+
+Get-ADComputer -Filter {ServicePrincipalName -like "*"} -Property ServicePrincipalName | ForEach-Object {
+    foreach ($spn in $_.ServicePrincipalName) {
+        $results += [PSCustomObject]@{
+            'Type'  = 'Computer'
+            'Name'  = $_.Name
+            'SPN'   = $spn
+        }
+    }
+}
+
+
+$results | Export-Csv -Path "SPNs.csv" -NoTypeInformation
+
+Write-Host "Export completed. Check SPNs.csv for the list of SPNs."
