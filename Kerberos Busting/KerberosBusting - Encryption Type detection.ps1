13e43825c76f64eb650f5f8a3ed40127941f55fc
Itay Bar <95435881+Itayb3@users.noreply.github.com>
2023-08-28 16:30:41 +0300

    Add files via upload

---
+++ Kerberos Busting/KerberosBusting - Encryption Type detection.ps1
@@ -0,0 +1,9 @@
+﻿$tickets = klist
+$lines = $tickets -split '\r\n'
+$encryptionTypes = @()
+
+foreach ($line in $lines) {
+    if ($line -match 'Etype \(skey, tkt\)') {
+        $encryptionTypes += ($line -split ':')[-1].Trim()
+    }
+}
